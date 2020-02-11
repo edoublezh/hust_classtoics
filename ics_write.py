a539ce3301b1d4dfbacfb4ec8af28e7b9d728a05
edouble <1171574037@qq.com>
2020-02-11 20:01:53 +0800

    Initial commit

---
+++ ics_write.py
@@ -0,0 +1,76 @@
+import datetime,os
+class Event:
+    def __init__(self,kwargs):
+        self.event_data = kwargs
+
+
+    def __turn_to_string__(self):
+        self.event_text = "BEGIN:VEVENT\n"
+        for item,data in self.event_data.items():
+            item = str(item).replace("_","-")
+            if item not in ["ORGANIZER","DTSTART","DTEND"]:
+                self.event_text += "%s:%s\n"%(item,data)
+            else:
+                self.event_text += "%s;%s\n"%(item,data)
+        self.event_text += "END:VEVENT\n"
+        return self.event_text
+class Calendar:
+    """
+    日历对象
+    """
+    def __init__(self,calendar_name="My Calendar"):
+        self.__events__ = {}
+        self.__event_id__ = 0
+        self.calendar_name = calendar_name
+    def add_event(self,**kwargs):
+        event = Event(kwargs)
+        event_id = self.__event_id__
+        self.__events__[self.__event_id__] = event
+        self.__event_id__ += 1
+        return event_id
+    def modify_event(self,event_id,**kwargs):
+        for item,data in kwargs.items():
+            self.__events__[event_id].event_data[item] = data
+    def remove_event(self,event_id):
+        self.__events__.pop(event_id)
+    def get_ics_text(self):
+        self.__calendar_text__ = """BEGIN:VCALENDAR\nPRODID:-//ZHONG_BAI_REN//APPGENIX-SOFTWARE//\nVERSION:2.0\nCALSCALE:GREGORIAN\nMETHOD:PUBLISH\nX-WR-CALNAME:%s\nX-WR-TIMEZONE:null\n"""%self.calendar_name
+        for key,value in self.__events__.items():
+            self.__calendar_text__ += value.__turn_to_string__()
+        self.__calendar_text__ += "END:VCALENDAR"
+        return self.__calendar_text__
+    def save_as_ics_file(self):
+        ics_text = self.get_ics_text()
+        open("%s.ics"%self.calendar_name,"w",encoding="utf8").write(ics_text)#使用utf8编码生成ics文件，否则日历软件打开是乱码
+
+    def open_ics_file(self):
+        os.system("%s.ics"%self.calendar_name)
+def add_event(cal, SUMMARY, DTSTART, DTEND, DESCRIPTION, LOCATION):
+    """
+    向Calendar日历对象添加事件的方法
+    :param cal: calender日历实例
+    :param SUMMARY: 事件名
+    :param DTSTART: 事件开始时间
+    :param DTEND: 时间结束时间
+    :param DESCRIPTION: 备注
+    :param LOCATION: 时间地点
+    :return:
+    """
+    time_format = "TZID=Asia/Shanghai:{date.year}{date.month:0>2d}{date.day:0>2d}T{date.hour:0>2d}{date.minute:0>2d}00"
+    dt_start = time_format.format(date=DTSTART)
+    dt_end = time_format.format(date=DTEND)
+    create_time = datetime.datetime.today().strftime("%Y%m%dT%H%M%SZ")
+    cal.add_event(
+        DTSTAMP=create_time,
+        SUMMARY=SUMMARY,
+        DTSTART=dt_start,
+        DTEND=dt_end,
+        UID="{}_edouble".format(dt_start),
+        SEQUENCE="0",
+        CREATED=create_time,
+        DESCRIPTION=DESCRIPTION,
+        LAST_MODIFIED=create_time,
+        LOCATION=LOCATION,
+        STATUS="CONFIRMED",
+        TRANSP="OPAQUE"
+    )
---
+++ main.py
@@ -0,0 +1,113 @@
+import urllib
+
+import ics_write
+from selenium import webdriver
+import time
+import datetime
+import urllib
+import urllib.request
+import json
+
+# browser = webdriver.Chrome()
+cookie_send = 'JSESSIONID=r4S2geDFr-1y5jz30EJX0y0alObdfe5vUQPDMbW8QO-bb_84vaws!-454866768;username=U201714656;BIGipServerpool_122.205.11.9_80=2047478282.22811.0000';
+
+
+def get_cookie(stuid, password):
+    browser = webdriver.Chrome()
+    browser.delete_all_cookies()
+    browser.get('https://pass.hust.edu.cn/cas/login?service=http%3A%2F%2Fhub.hust.edu.cn%2Fhustpass.action')
+    time.sleep(1)
+    # 未进入则刷新
+    if browser.title != '智慧华中大 | 统一身份认证系统':
+        browser.refresh()
+        time.sleep(2)
+    elem = browser.find_element_by_id('un')
+    elem.send_keys(stuid)
+    elem = browser.find_element_by_id('pd')
+    elem.send_keys(password)
+    browser.find_element_by_id('index_login_btn').click()
+    time.sleep(1)
+    browser.switch_to.window(browser.window_handles[-1])
+    list_cookies = browser.get_cookies()
+
+    global cookie_send
+    cookie_send = "%s=%s;%s=%s;%s=%s" % (
+    list_cookies[2]['name'], list_cookies[2]['value'], list_cookies[0]['name'], list_cookies[0]['value'],
+    list_cookies[1]['name'], list_cookies[1]['value'])
+    # browser.quit()
+
+
+def getcourse(begin_time, end_time):
+    print('等待抓取课程')
+
+    headers4 = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763',
+        'Accept': 'application/json, text/javascript, */*; q=0.01',
+        'Accept-Language': 'en-US,en;q=0.8,zh-Hans-CN;q=0.5,zh-Hans;q=0.3',
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'X-Requested-With': 'XMLHttpRequest',
+        'Accept-Encoding': 'gzip, deflate',
+        'Content-Length': '30',
+        'Host': 'hubs.hust.edu.cn',
+        'Connection': 'Keep-Alive',
+        'Pragma': 'no-cache',
+        'Cookie': None
+
+    }
+
+    headers4['Cookie'] = cookie_send
+    params4 = {
+        'start': '2019-09-26',
+        'end': '2019-11-07'
+    }
+    params4['start']=begin_time
+    params4['end']=end_time
+    bdata4 = urllib.parse.urlencode(params4).encode('ascii')
+
+    url = 'http://hubs.hust.edu.cn/aam/score/CourseInquiry_ido.action'
+    req4 = urllib.request.Request(url, bdata4, headers=headers4)
+    response4 = urllib.request.urlopen(req4)
+    data4 = json.loads(response4.read().decode('utf-8'))
+    # js=json.dumps(data4,ensure_ascii=False,indent=2)
+    # print(js)
+    with open('lessons.json', 'w') as f:
+        f.write(json.dumps(data4, ensure_ascii=False))
+
+
+def trans_ics():
+    with open("lessons.json","r") as f:
+        lessons=json.loads(f.read())
+    lesson_num=len(lessons)
+    a=eval(lessons[0]['txt'])
+    calendar = ics_write.Calendar(calendar_name="test")
+    for i in range(0,lesson_num):
+        if i%2:
+            lessonstatus = eval(lessons[i]['txt'])
+            a = lessons[i]['start']
+            starttime = datetime.datetime.strptime(lessons[i]['start'], "%Y-%m-%d %H:%M")
+            endtime = datetime.datetime.strptime(lessons[i]['end'], "%Y-%m-%d %H:%M")
+            ics_write.add_event(calendar,
+                                SUMMARY=lessons[i]['title'],
+                                DTSTART=starttime,
+                                DTEND=endtime,
+                                DESCRIPTION=lessonstatus['JGXM'],
+                                LOCATION=lessonstatus['JSMC'])
+
+    print(calendar.get_ics_text())
+    calendar.save_as_ics_file()
+
+
+def save(string):
+    f = open("class.ics", 'wb')
+    f.write(string.encode("utf-8"))
+    f.close()
+
+
+
+
+
+
+if __name__ == '__main__':
+    get_cookie('学号', '密码')
+    getcourse('2020-02-11', '2020-07-00')
+    trans_ics()
